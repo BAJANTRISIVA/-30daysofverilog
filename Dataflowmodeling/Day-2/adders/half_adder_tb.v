276d3a7372a1d251982ae44963805db64ed37721
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-28 11:31:51 +0530

    Update half_adder_tb.v

--- Dataflowmodeling/Day-2/adders/half_adder_tb.v
+++ Dataflowmodeling/Day-2/adders/half_adder_tb.v
@@ -1,1 +1,16 @@
-
+module half_add_tb;
+  reg a,b;
+  wire sum,carry;
+  half_add_dut siva (.a(a),.b(b),.sum(sum),.carry(carry));
+  initial 
+      begin
+	      $monitor ("a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);    
+       		 a=0;b=0;
+    			 #20;
+    			 a=0;b=1; 
+    			 #20
+    			 a=1;b=0;
+    			 #20
+    			 a=1;b=1;        	 	    
+	    end
+endmodule
