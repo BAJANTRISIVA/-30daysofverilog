2c2715b163c12188dac292eecc55d046783cad59
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-28 11:06:07 +0530

    Update half_adder.v

--- Dataflowmodeling/Day-2/adders/half_adder.v
+++ Dataflowmodeling/Day-2/adders/half_adder.v
@@ -1,1 +1,13 @@
+module half_add_dut (a,b,sum,carry);
+  input a,b;
+  output reg sum,carry;
 
+  /*assign sum = a^b;
+  assign carry = a&b; */
+
+	always @(a,b)  //procedural assignm
+		begin
+  		sum<=a^b;
+  		carry<=a&b;
+		end
+endmodule
