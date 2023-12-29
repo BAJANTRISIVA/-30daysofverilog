9a2636b19fcea7888cd2828b4be3c3971fab6c34
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-29 12:47:42 +0530

    Update full_sub.v

--- Dataflowmodeling/Day-3/subtractor/full_sub.v
+++ Dataflowmodeling/Day-3/subtractor/full_sub.v
@@ -1,1 +1,15 @@
+// A full subtractor is a combinational circuit that performs subtraction involving three bits, namely a, b, and c, and it produces two outputs.
+// difference and borrow.
 
+module half_sub(a,b,c,dif,bor);
+  input a,b,c;
+ // output dif,bor;
+  output reg dif,bor;
+  //assign dif=a^b^c;
+ // assign bor=!a&b|!a&c|b&c;
+  always @(*)
+    begin
+        dif <= a^b^c;
+        bor <= !a&b|!a&c|b&c;
+    end
+endmodule
