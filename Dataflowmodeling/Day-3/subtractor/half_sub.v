06807240beb75952b9b0d3a4433fda86032c5515
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-29 12:25:55 +0530

    Update half_sub.v

--- Dataflowmodeling/Day-3/subtractor/half_sub.v
+++ Dataflowmodeling/Day-3/subtractor/half_sub.v
@@ -1,1 +1,14 @@
-
+// A half subtractor is a combinational circuit which is used to perform subtraction of two bits and it has two inputs and two outputs
+// namely difference and borrow
+module half_sub(a,b,dif,bor);
+  input a,b;
+ // output dif,bor;
+  output reg dif,bor;
+  //assign dif=a^b;
+ // assign bor=!a&b;
+  always @(*)
+    begin
+        dif <= a^b;
+        bor <= !a&b;
+    end
+endmodule
