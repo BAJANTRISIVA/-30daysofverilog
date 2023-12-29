e99d1611e0ced5218914099d2a375edd4ea6370f
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-30 01:07:29 +0530

    Update comparator_1bit.v

--- Dataflowmodeling/Day-8/comparator_1bit.v
+++ Dataflowmodeling/Day-8/comparator_1bit.v
@@ -1,1 +1,11 @@
+// A digital comparator or magnitude comparator is a hardware electronic device that takes two numbers as input in binary form and determines 
+// whether one number is greater than, less than or equal to the other number.Comparators are used in central processing units (CPUs) 
+// and microcontrollers (MCUs).
+module comp_1_dut(a,b,y0,y1,y2);
+  input a,b;
+  output y0,y1,y2;
+  assign y0=a&~b;
+  assign y1=a~^b;
+  assign y2=~a&b;
+endmodule
 
