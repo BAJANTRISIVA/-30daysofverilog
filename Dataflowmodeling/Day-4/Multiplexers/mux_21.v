31f58f4cdbcc6b7cd2cd01e6e9d85eed1e480ee2
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-29 13:30:20 +0530

    Update mux_21.v

--- Dataflowmodeling/Day-4/Multiplexers/mux_21.v
+++ Dataflowmodeling/Day-4/Multiplexers/mux_21.v
@@ -1,1 +1,9 @@
+// Multiplexer is a combinational circuit which has many data inputs and produce single output based on the select line.It also called selector or parallel to serila converter
 
+module mux_21(a,b,s,y);
+  input a,b;
+  input s;
+  output y;
+//  assign y=s?b:a;  //conditional statement
+  assign y = a&!s | b&s;
+endmodule
