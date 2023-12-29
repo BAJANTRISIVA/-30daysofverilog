2baa3d89cf75f4a00e990ee3b34b6070673e6310
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-29 12:15:19 +0530

    Update half_adder.v

--- Dataflowmodeling/Day-2/adders/half_adder.v
+++ Dataflowmodeling/Day-2/adders/half_adder.v
@@ -1,3 +1,6 @@
+// Half adder is a combinational circuit which computer binary addition of two binary inputs. 
+// It is one of the basic combinational circuit in which we have combination of two gates (ex-or gate, and gate)
+
 module half_add_dut (a,b,sum,carry);
   input a,b;
   output reg sum,carry;
