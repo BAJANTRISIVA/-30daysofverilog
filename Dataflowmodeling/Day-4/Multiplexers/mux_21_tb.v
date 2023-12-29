c9554bd374a9a3a2381473a0a5e8450f811ab34c
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-29 14:06:17 +0530

    Update mux_21_tb.v

--- Dataflowmodeling/Day-4/Multiplexers/mux_21_tb.v
+++ Dataflowmodeling/Day-4/Multiplexers/mux_21_tb.v
@@ -2,13 +2,14 @@ module mux_21_tb;
   reg a,b;
   reg s;
   wire y;
-  integer i,j;
+  integer i;
+  integer j;
   mux_21 m1(a,b,s,y);
   initial begin
     $monitor("a=%0d,b=%0d,s=%0d,y=%0d",a,b,s,y);
-   a=1;b=0;s=0;  //directed testbench
-#2 a=0;b=1;s=1;
-    /* for(i=0;i<=4;i=i+1)  //using for loop
+ /*  a=1;b=0;s=0;
+  #2 a=0;b=1;s=1;*/
+  for(i=0;i<=4;i=i+1)
       begin
         {a,b}=i+1;
       end
@@ -17,6 +18,7 @@ module mux_21_tb;
             {s}=j;
             #5;
           end
-#10;*/
+          #10;
   end
+  initial #100 $stop;
 endmodule
