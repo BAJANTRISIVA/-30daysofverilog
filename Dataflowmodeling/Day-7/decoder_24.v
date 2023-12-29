7c65130c601843c1ca7ead5315b54f6870d09a1e
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-30 01:02:45 +0530

    Update decoder_24.v

--- Dataflowmodeling/Day-7/decoder_24.v
+++ Dataflowmodeling/Day-7/decoder_24.v
@@ -1,3 +1,6 @@
+// A decoder is a multiple-input, multiple-output logic circuit that converts coded inputs into coded outputs,
+// where the input and output codes are different. It has N input lines and 2 power n output lines
+
 module decoder_24(a,b,y0,y1,y2,y3);
   input a,b;
   output y0,y1,y2,y3;
