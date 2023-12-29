dbe884ce48fb6dc02e9264fac722bd6a8a82978f
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-30 00:29:37 +0530

    Update encoder_42_tb.v

--- Dataflowmodeling/Day-6/encoder_42_tb.v
+++ Dataflowmodeling/Day-6/encoder_42_tb.v
@@ -1,1 +1,12 @@
-
+module encoder_42_tb;
+  reg a,b,c,d;
+  wire y0,y1;
+  encoder_42 en(a,b,c,d,y0,y1);
+  initial begin
+    $monitor("a=%0d,b=%0d,c=%0d,d=%0d,y0=%0d,y1=%0d",a,b,c,d,y0,y1);
+      a=1;b=0;c=0;d=0;
+  #2  a=0;b=1;c=0;d=0;
+  #2  a=0;b=0;c=1;d=0;
+  #2  a=0;b=0;c=0;d=1;
+  end
+endmodule
