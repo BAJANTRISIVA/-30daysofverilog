19617717b0206f2ebe4e31f9e9ebccfb964aa0aa
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-29 12:32:55 +0530

    Update half_sub_tb.v

--- Dataflowmodeling/Day-3/subtractor/half_sub_tb.v
+++ Dataflowmodeling/Day-3/subtractor/half_sub_tb.v
@@ -1,1 +1,12 @@
-
+module half_sub_tb;
+  reg a,b;
+  wire dif,bor;
+  half_sub h1(a,b,dif,bor);
+  initial begin
+      $monitor("a=%0d,b=%0d,dif=%0d,bor=%0d",a,b,dif,bor);
+      a=0;b=0;
+  #4  a=0;b=1;
+  #4  a=1;b=0;
+  #4  a=1;b=1;
+  end
+endmodule
