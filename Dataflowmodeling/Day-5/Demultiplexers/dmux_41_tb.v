7d26266d74bf9be3d108567a6fb8b82eec65a820
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-29 16:35:07 +0530

    Update dmux_41_tb.v

--- Dataflowmodeling/Day-5/Demultiplexers/dmux_41_tb.v
+++ Dataflowmodeling/Day-5/Demultiplexers/dmux_41_tb.v
@@ -1,1 +1,15 @@
+module demux_41_tb;
+  reg i;
+  reg s0,s1;
+  wire [3:0]y;
+  dmux_41 d1(.i(i),.s0(s0),.s1(s1),.y(y));
+  initial begin
+    $monitor("i=%b,s0=%b,s1=%b,y=%b",i,s0,s1,y);
+         {i,s0,s1} = 3'b100;
+    #2;  {i,s0,s1} = 3'b101;
+    #2;  {i,s0,s1} = 3'b110;
+    #2;  {i,s0,s1} = 3'b111;
+  end
+endmodule
+
 
