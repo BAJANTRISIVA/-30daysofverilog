86fb0c85e3e56e1291502b67cd30782bdb3e6232
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2023-12-30 00:24:22 +0530

    Update encoder_42.v

--- Dataflowmodeling/Day-6/encoder_42.v
+++ Dataflowmodeling/Day-6/encoder_42.v
@@ -1,1 +1,8 @@
+// An Encoder is a combinational circuit that performs the reverse operation of Decoder. It has maximum of 2n input lines and 'n' output lines. 
 
+module encoder_42(a,b,c,d,y0,y1);
+  input a,b,c,d;
+  output y0,y1;
+  assign y0 = i2 | i3;
+  assign y1 = i1 | i3;
+endmodule
