2ef572935b7413c4aa2801c54585ebddf97361b7
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2024-01-06 20:38:44 +0530

    Update all_logic_gates.v

--- Gatelevelmodeling/Day-9/all_logic_gates.v
+++ Gatelevelmodeling/Day-9/all_logic_gates.v
@@ -1,1 +1,11 @@
-
+module all_logic_gates_dut (a,b,y);
+      input a,b;
+  	  output y;
+      not n1 (y,a,b);
+      and a1 (y,a,b);
+      or o1 (y,a,b);
+      nand n2 (y,a,b);
+      nor n3 (y,a,b);
+      xor x1 (y,a,b);
+      xnor x2(y,a,b);
+endmodule
