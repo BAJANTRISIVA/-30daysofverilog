70c191f2800651da9b23343310b764c5d59f9041
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2024-01-07 14:02:43 +0530

    Update full_adder_tb.v

--- Gatelevelmodeling/Day-9/Day-10/adders/full_adder_tb.v
+++ Gatelevelmodeling/Day-9/Day-10/adders/full_adder_tb.v
@@ -1,11 +1,30 @@
-module full_adder_tb;
+module full_adder_tb; 
   reg a,b,c;
-  wire s,carry;
-  full_add_dut fa(.a(a),.b(b),.c(c),.s(s),.carry(carry));
+  wire sum,carry;
+ // Instantiate the DUT
+ full_add_dut dut(.a(a),
+                  .b(b),
+                  .c(c),
+                  .sum(sum),
+                  .carry(carry)
+                );
   initial begin
-    repeat(5) begin
-    {a,b,c}=$random;
-    $display("a=%b,b=%b,c=%b,s=%b,carry=%b",a,b,c,s,carry);
-    end
+       a = 0; b = 0; c = 0;
+       #10;
+       $display("Test Case 2: a=%b, b=%b, c=%b, sum=%b, carry=%b",a,b,c,sum,carry);
+    
+       a = 1; b = 0; c = 0;
+       #10;
+       $display("Test Case 2: a=%b, b=%b, c=%b, sum=%b, carry=%b",a,b,c,sum,carry);
+    
+       a = 1; b = 1; c = 0;
+       #10;
+       $display("Test Case 3: a=%b, b=%b, c=%b, sum=%b, Cout=%b",a,b,c,sum,carry);
+
+       a = 1; b = 1; c = 1;
+       #10;
+       $display("Test Case 4: a=%b, b=%b, c=%b, sum=%b, Cout=%b", a,b,c,sum,carry);
+       $finish;
   end
 endmodule
+
