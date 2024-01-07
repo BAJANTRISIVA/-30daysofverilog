b309ddb456be9ff46b156155b564bd9bcf8b69c1
BAJANTRISIVA <147977722+BAJANTRISIVA@users.noreply.github.com>
2024-01-07 11:03:02 +0530

    Update half_adder_tb.v

--- Gatelevelmodeling/Day-9/Day-10/adders/half_adder_tb.v
+++ Gatelevelmodeling/Day-9/Day-10/adders/half_adder_tb.v
@@ -1,1 +1,9 @@
-
+module half_add_tb;
+  reg a,b;
+  wire s,c;
+  half_add_dut sai (.a(a),.b(b),.s(s),.c(c));
+  initial begin
+	  repeat(8) begin
+      */{a,b}=$random;
+     $display("a=%b,b=%b,s=%b,c=%b",a,b,s,c);
+endmodule
